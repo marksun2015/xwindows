9a77cede892315ca188b51c6c6c7c8e59fb48ea6
Mark Sun <mark@weintek.com>
2022-06-08 14:50:02 +0800

    Modify xfakexy.cpp

--- xfakexy.cpp
+++ xfakexy.cpp
@@ -1,24 +1,16 @@
 /*
- * instruction
- * $ xfakexe 60 50
- *
-*/
+ *  https://www.linuxquestions.org/questions/programming-9/simulating-a-mouse-click-594576/
+ */
 
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <unistd.h>
-
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-
-void mouseClick(int button)
+void mouseClick(int button,Display *display)
 {
-    Display *display = XOpenDisplay(NULL);
-
     XEvent event;
 
     if(display == NULL)
@@ -33,18 +25,38 @@ void mouseClick(int button)
     event.xbutton.button = button;
     event.xbutton.same_screen = True;
 
-    XQueryPointer(display, RootWindow(display, DefaultScreen(display)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
+    XQueryPointer(display, 
+            RootWindow(display, DefaultScreen(display)), 
+            &event.xbutton.root, 
+            &event.xbutton.window, 
+            &event.xbutton.x_root, 
+            &event.xbutton.y_root, 
+            &event.xbutton.x, 
+            &event.xbutton.y, 
+            &event.xbutton.state);
 
     event.xbutton.subwindow = event.xbutton.window;
+
+    //printf("event xroot:%d, event yroot:%d \n",event.xbutton.x_root, event.xbutton.y_root);
+    //printf("event x:%d, event y:%d \n",event.xbutton.x, event.xbutton.y);
 
     while(event.xbutton.subwindow)
     {
         event.xbutton.window = event.xbutton.subwindow;
 
-        XQueryPointer(display, event.xbutton.window, &event.xbutton.root, &event.xbutton.subwindow, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
+        XQueryPointer(display, 
+                event.xbutton.window, 
+                &event.xbutton.root, 
+                &event.xbutton.subwindow, 
+                &event.xbutton.x_root, 
+                &event.xbutton.y_root, 
+                &event.xbutton.x, 
+                &event.xbutton.y, 
+                &event.xbutton.state);
     }
 
-    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Error\n");
+    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) 
+        fprintf(stderr, "Error\n");
 
     XFlush(display);
 
@@ -53,23 +65,30 @@ void mouseClick(int button)
     event.type = ButtonRelease;
     event.xbutton.state = 0x100;
 
-    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Error\n");
-
-    XFlush(display);
-
-    XCloseDisplay(display);
+    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) 
+        fprintf(stderr, "Error\n");
 }
+
 int main(int argc, char * argv[]) {
 
     int x , y;
+
+    if(argc < 3) {
+        printf("  send button1 please input: fakexy x y \n");
+        printf("     capture button, x need larger than window border: 1 \n");
+        printf("     capture button, y need larger than title height: 24 \n");
+        return 0;
+    }
+
     x = atoi(argv[1]);
     y = atoi(argv[2]);
-    Display *display = XOpenDisplay(0);
 
+    Display *display = XOpenDisplay(0);
     Window root = DefaultRootWindow(display);
     XWarpPointer(display, None, root, 0, 0, 0, 0, x, y);
-    mouseClick(Button1);
+    mouseClick(Button1, display);
     XFlush(display);
     XCloseDisplay(display);
+
     return 0;
 }
